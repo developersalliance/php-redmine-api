240ed8afa95236d94212e2553710be38db4f1717
Dmitriy Gallyamov <dmitrijs.gallamovs@gmail.com>
2019-11-29 18:02:09 +0200

    Added unsassigned issues

--- dailyProjectReports.php
+++ dailyProjectReports.php
@@ -10,6 +10,46 @@ $headerMessage = sprintf('%s (%s)', '## Daily Ticket Report', date('Y-m-d'));
 echo $headerMessage;
 $channel->postMessage($headerMessage);
 
+$userIssues = $client->issue->all(
+    [
+        'status_id' => 'open',
+        'limit' => '100'
+    ]
+);
+
+$unassignedIssues = [];
+foreach ($userIssues['issues'] as $issue) {
+    if (!isset($issue['assigned_to'])) {
+        $timeEntry = getTimeEntries($issue['id'], $client);
+        $estimatedTime = isset($issue['estimated_hours']) ? $issue['estimated_hours'] : 0;
+
+        $format = '[Ticket](%s/issues/%s) [%s][%s][%s] (%s) estimated time is %s and spent %s.';
+
+        if ($timeEntry > $estimatedTime | 0 === $estimatedTime) {
+            $format = sprintf('*%s*', $format);
+        }
+
+        $unassignedIssues[] = sprintf(
+            $format,
+            $url,
+            $issue['id'],
+            $issue['id'],
+            $issue['project']['name'],
+            $issue['subject'],
+            $issue['status']['name'],
+            $estimatedTime,
+            $timeEntry
+        );
+    }
+}
+
+$message = "\n".'*Unassigned*'."\n\n";
+foreach ($unassignedIssues as $item) {
+    $message .= $item."\n";
+}
+
+$channel->postMessage($message);
+
 foreach ($group['group']['users'] as $user) {
     $status = [];
     $userIssues = $client->issue->all(
